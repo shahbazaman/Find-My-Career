fe13ff156532af8e053e6144689a1e91ad58ba3a
shahbazaman <shabushahbaz123@gmail.com>
2026-02-09 15:50:15 +0530

    updated route

--- server/routes/emailTestRoutes.js
+++ server/routes/emailTestRoutes.js
@@ -1,9 +1,9 @@
 import express from "express";
-import protect from "../middleware/authMiddleware.js";
+// import protect from "../middleware/authMiddleware.js";
 import { sendTestEmail } from "../controllers/emailTestController.js";
 
 const router = express.Router();
 
-router.post("/email/test", protect, sendTestEmail);
+router.post("/email/test", sendTestEmail);
 
 export default router;
